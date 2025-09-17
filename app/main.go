68500f5a792af30fc14777e09ceb3fc3db64531b
cesarleyton <cesarleyton549@gmail.com>
2025-09-17 13:43:11 -0500

    create socket to listen to tcp connections

--- app/main.go
+++ app/main.go
@@ -6,17 +6,39 @@ import (
 	"os"
 )
 
+type Response struct {
+	startLine []string
+	headers   []string
+	body      []string
+}
+
+type Request struct {
+	startLine []string
+	headers   []string
+	body      []string
+}
+
 func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
+	fmt.Println("server", l)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	c, err := l.Accept()
+	fmt.Println("c", c)
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleConn(c)
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Println("listening")
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
