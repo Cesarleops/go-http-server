c2ce32df33b3b848f2f73c2aa086e0dfb92b9ee0
cesarleyton <cesarleyton549@gmail.com>
2025-09-22 13:29:28 -0500

    close connection if request connection header is set to close

--- app/main.go
+++ app/main.go
@@ -34,105 +34,6 @@ type ResponseStartLine struct {
 	reason   string
 }
 
-func main() {
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	for {
-		c, err := l.Accept()
-		fmt.Println("Accepting...")
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-
-		go handleConn(c)
-	}
-
-}
-
-func handleConn(conn net.Conn) {
-
-	for {
-		fmt.Println("handling...")
-
-		buf := make([]byte, 1024)
-
-		b, err := conn.Read(buf)
-
-		if b == 0 {
-			break
-		}
-
-		if err != nil {
-			conn.Write([]byte("HTTP/1.1 404 Not Found"))
-			break
-		}
-
-		data := string(buf)
-
-		request := &Request{
-			headers: make(map[string]string),
-		}
-
-		request.build(data)
-
-		response := &Response{
-			headers: make(map[string]string),
-		}
-
-		target := request.startLine.target
-
-		paths := strings.Split(strings.TrimPrefix(target, "/"), "/")
-
-		if strings.HasPrefix(target, "/files") {
-
-			fileName := paths[1]
-			directory := os.Args[2]
-
-			if request.startLine.method == "POST" {
-				response.parseStartLine("201", "Created")
-				err := os.WriteFile(directory+"/"+fileName, []byte(request.body), 0644)
-				if err != nil {
-					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-				}
-				conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
-
-			} else {
-				f, err := os.ReadFile(directory + "/" + fileName)
-
-				if err != nil {
-					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-				}
-				response.build(*request, string(f), "application/octet-stream", "200", "OK")
-				response := response.Bytes()
-				conn.Write([]byte(response))
-			}
-		} else if target == "/user-agent" {
-			content := request.headers["User-Agent"]
-			response.build(*request, content, "text/plain", "200", "OK")
-			response := response.Bytes()
-			conn.Write([]byte(response))
-		} else if paths[0] == "echo" {
-			fmt.Println("echoing...", paths[1])
-			response.build(*request, paths[1], "text/plain", "200", "OK")
-			response := response.Bytes()
-			conn.Write([]byte(response))
-		} else if target == "/" {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-
-		}
-	}
-
-}
-
 func (r *Request) build(data string) {
 	r.parseHeaders(data)
 	r.parseStartLine(data)
@@ -196,6 +97,10 @@ func (r *Response) parseHeaders(request Request, contentType string, contentLeng
 	if encodings, exists := request.headers["Accept-Encoding"]; exists && strings.Contains(encodings, "gzip") {
 		r.headers["Content-Encoding"] = " " + "gzip"
 	}
+
+	if _, ok := request.headers["Connection"]; ok {
+		r.headers["Connection"] = " " + "close"
+	}
 }
 
 func (r *Response) parseBody(content string) {
@@ -236,4 +141,130 @@ func (r Response) Bytes() []byte {
 
 	// combine headers (as bytes) + body (as raw bytes)
 	return append([]byte(headers), r.body...)
+}
+
+func (r Response) SendResp(conn net.Conn) {
+
+}
+
+func main() {
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConn(c)
+	}
+
+}
+
+func handleConn(conn net.Conn) {
+
+	for {
+
+		buf := make([]byte, 1024)
+
+		b, err := conn.Read(buf)
+
+		if b == 0 {
+			break
+		}
+
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 404 Not Found"))
+			break
+		}
+
+		data := string(buf)
+
+		request := &Request{
+			headers: make(map[string]string),
+		}
+
+		request.build(data)
+
+		response := &Response{
+			headers: make(map[string]string),
+		}
+
+		target := request.startLine.target
+
+		paths := strings.Split(strings.TrimPrefix(target, "/"), "/")
+
+		if strings.HasPrefix(target, "/files") {
+
+			fileName := paths[1]
+			directory := os.Args[2]
+			if request.startLine.method == "POST" {
+				response.parseStartLine("201", "Created")
+				err := os.WriteFile(directory+"/"+fileName, []byte(request.body), 0644)
+				if err != nil {
+					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				}
+				conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
+
+			} else {
+				f, err := os.ReadFile(directory + "/" + fileName)
+
+				if err != nil {
+					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				}
+				response.build(*request, string(f), "application/octet-stream", "200", "OK")
+				response := response.Bytes()
+				conn.Write([]byte(response))
+			}
+
+		} else if target == "/user-agent" {
+
+			content := request.headers["User-Agent"]
+			response.build(*request, content, "text/plain", "200", "OK")
+			v := response.headers["Connection"]
+			response := response.Bytes()
+			conn.Write([]byte(response))
+			fmt.Println("h1", v)
+			if strings.TrimSpace(v) == "close" {
+				fmt.Println("closing")
+				conn.Close()
+				break
+			}
+
+		} else if paths[0] == "echo" {
+
+			response.build(*request, paths[1], "text/plain", "200", "OK")
+			v := response.headers["Connection"]
+			if strings.TrimSpace(v) == "close" {
+				fmt.Println("closing")
+				conn.Close()
+				break
+			}
+
+			response := response.Bytes()
+			conn.Write([]byte(response))
+
+		} else if target == "/" {
+			response.build(*request, "", "", "200", "OK")
+			v := response.headers["Connection"]
+			conn.Write(response.Bytes())
+			if strings.TrimSpace(v) == "close" {
+				fmt.Println("closing")
+				conn.Close()
+				break
+			}
+
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+		}
+	}
+
 }
